e034fdc28256d463c530b0fdaa8400827425bd1b
731614105013 <gomathiammupk29@gmail.com>
2017-07-05 09:40:41 +0530

    Create Print.java

---
+++ Print.java
@@ -0,0 +1,20 @@
+# print
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class Print
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    int a=5,i;
+    for(i=1;i<=a;i++)
+    {
+      System.out.println("Hello");
+      }
+      }
+      }
+      
+
+
+        
